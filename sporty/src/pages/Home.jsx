01c8a86d54267ff4a96cecd80584a1c843f2d67e
leticia <leticiaae@hotmail.com.br>
2023-11-12 23:43:12 -0300

    feat: starting home page

--- sporty/src/pages/Home.jsx
+++ sporty/src/pages/Home.jsx
@@ -1,7 +1,26 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Flex, Stack, Text } from "@chakra-ui/react";
 
 export function Home() {
     return (
-        <Text>OI</Text>
+        <Flex w="full" h="100vh">
+            <Flex flexDirection="column" flex={1} bgColor="light.400" >
+                <Text fontSize={28} textAlign="center" fontWeight="bold" color="primary.400">SPORTY</Text>
+                <Stack flex={1} justifyContent="center">
+
+                    <Button color="light.400" bgColor="transparent">
+                        Todos os torneios
+                    </Button>
+                    <Button >
+                        Todos os torneios
+                    </Button>
+                    <Button >
+                        Todos os torneios
+                    </Button>
+                </Stack>
+            </Flex>
+            <Flex flex={4} bgColor="primary.400">
+
+            </Flex>
+        </Flex>
     )
 }
