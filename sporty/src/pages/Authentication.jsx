b0fbd0434dc1de75ff5a3d4b8af8d100e27c0cee
leticia <leticiaae@hotmail.com.br>
2023-11-12 21:29:27 -0300

    feat: added tabs for sign in and sign up

--- sporty/src/pages/Authentication.jsx
+++ sporty/src/pages/Authentication.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Controller, useForm } from "react-hook-form";
 import Esporte from "../../public/assets/esporte.png";
 import { Eye, EyeClosed, LockKey, SignIn } from "@phosphor-icons/react"
-import { Button, Center, Flex, FormControl, FormErrorMessage, Input, InputGroup, InputLeftElement, InputRightElement, Stack, Text } from "@chakra-ui/react";
+import { Button, Center, Flex, FormControl, FormErrorMessage, Input, InputGroup, InputLeftElement, InputRightElement, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
 
 
 export function Authetication() {
@@ -29,8 +29,7 @@ export function Authetication() {
     },
   });
 
-  const onSubmit = (data) => {
-    // console.log(data?.name)
+  const onSignUp = (data) => {
     if (data.name === user.name && data.login === user.login && data.password === user.password) {
       navigate("Home")
     } else {
@@ -39,6 +38,24 @@ export function Authetication() {
         type: "validate",
         message: "Credenciais inválidas"
       });
+
+      setError("login", {
+        type: "validate",
+        message: "Credenciais inválidas",
+      });
+
+      setError("password", {
+        type: "validate",
+        message: "Credenciais inválidas",
+      });
+    }
+  };
+
+  const onSignIn = (data) => {
+    if (data.login === user.login && data.password === user.password) {
+      navigate("Home")
+    } else {
+      // Autenticação falhou
 
       setError("login", {
         type: "validate",
@@ -63,106 +80,192 @@ export function Authetication() {
           <Text fontWeight="semibold" fontSize={28}>Bem - vindo de volta</Text>
           <Text fontSize={16} whiteSpace="pre-line">Se cadastre e gerencie seus torneios diretamente{"\n"} de dentro da plataforma.</Text>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
 
-            <Stack justifyContent="center" bgColor="light.200" padding={5} rounded={8} boxShadow="2xl" m={50} >
+          <Stack justifyContent="center" bgColor="light.200" padding={5} rounded={8} boxShadow="2xl" m={50} >
+            <Tabs variant='enclosed' isFitted colorScheme="primary" >
+              <TabList>
+                <Tab>Sign up</Tab>
+                <Tab>Sign in</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <form onSubmit={handleSubmit(onSignUp)}>
+                    <Controller
+                      name="name"
+                      control={control}
+                      rules={{ required: "Nome obrigatório." }}
+                      render={({ field }) => (
 
-              <Controller
-                name="name"
-                control={control}
-                rules={{ required: "Nome obrigatório." }}
-                render={({ field }) => (
+                        <FormControl isInvalid={errors?.name}>
+                          <Text color="dark.300">Nome</Text>
+                          <InputGroup>
+                            <InputLeftElement pointerEvents='none'>
+                              {/* <LockKey size={24} /> */}
+                            </InputLeftElement>
+                            <Input focusBorderColor="primary.400" variant="outline" type='text' placeholder='Leticia Matos' {...field} />
+                          </InputGroup>
+                          {errors?.name && (
+                            <FormErrorMessage ml={5}>
+                              {errors?.name?.message}
+                            </FormErrorMessage>
+                          )}
+                        </FormControl>
+                      )}
+                    />
 
-                  <FormControl isInvalid={errors?.name}>
-                    <Text color="dark.300">Nome</Text>
-                    <InputGroup>
-                      <InputLeftElement pointerEvents='none'>
-                        {/* <LockKey size={24} /> */}
-                      </InputLeftElement>
-                      <Input focusBorderColor="primary.400" variant="outline" type='text' placeholder='Leticia Matos' {...field} />
-                    </InputGroup>
-                    {errors?.name && (
-                      <FormErrorMessage ml={5}>
-                        {errors?.name?.message}
-                      </FormErrorMessage>
-                    )}
-                  </FormControl>
-                )}
-              />
+                    <Controller
+                      name="login"
+                      control={control}
+                      rules={{ required: "Login obrigatório." }}
+                      render={({ field }) => (
+                        <FormControl isInvalid={errors?.login}>
 
-              <Controller
-                name="login"
-                control={control}
-                rules={{ required: "Login obrigatório." }}
-                render={({ field }) => (
-                  <FormControl isInvalid={errors?.login}>
+                          <Text color="dark.300">Login</Text>
+                          <InputGroup>
+                            <InputLeftElement pointerEvents='none'>
+                              {/* <LockKey size={76} /> */}
+                            </InputLeftElement>
+                            <Input focusBorderColor="primary.400" type='text' placeholder='Ex: lele123' {...field} />
+                          </InputGroup>
 
-                    <Text color="dark.300">Login</Text>
-                    <InputGroup>
-                      <InputLeftElement pointerEvents='none'>
-                        {/* <LockKey size={76} /> */}
-                      </InputLeftElement>
-                      <Input focusBorderColor="primary.400" type='text' placeholder='Ex: lele123' {...field} />
-                    </InputGroup>
+                          {errors.login && (
+                            <FormErrorMessage ml={5}>
+                              {errors?.login?.message}
+                            </FormErrorMessage>
+                          )}
+                        </FormControl>
+                      )}
+                    />
 
-                    {errors.login && (
-                      <FormErrorMessage ml={5}>
-                        {errors?.login?.message}
-                      </FormErrorMessage>
-                    )}
-                  </FormControl>
-                )}
-              />
-
-              <Controller
-                name="password"
-                control={control}
-                rules={{ required: "Senha é obrigatória." }}
-                render={({ field }) => (
-                  <FormControl isInvalid={errors.password}>
-                    <Text color="dark.300">Senha</Text>
-                    <InputGroup>
-                      <InputLeftElement color="primary.400" pointerEvents='none'>
-                        <LockKey size={24} style={{ color: "primary.400" }} />
-                      </InputLeftElement>
-                      <Input focusBorderColor="primary.400" type={!show ? 'password' : "text"} placeholder='Digite sua senha' {...field} />
-                      <InputRightElement
-                        onClick={() => setShow((prev) => !prev)}
-                        color="primary.400"
-                        // eslint-disable-next-line react/no-children-prop
-                        children={
-                          show ? (
-                            <Eye
-                              size={25}
-                              style={{ alignSelf: "center" }}
+                    <Controller
+                      name="password"
+                      control={control}
+                      rules={{ required: "Senha é obrigatória." }}
+                      render={({ field }) => (
+                        <FormControl isInvalid={errors.password}>
+                          <Text color="dark.300">Senha</Text>
+                          <InputGroup>
+                            <InputLeftElement color="primary.400" pointerEvents='none'>
+                              <LockKey size={24} style={{ color: "primary.400" }} />
+                            </InputLeftElement>
+                            <Input focusBorderColor="primary.400" type={!show ? 'password' : "text"} placeholder='Digite sua senha' {...field} />
+                            <InputRightElement
+                              onClick={() => setShow((prev) => !prev)}
+                              color="primary.400"
+                              // eslint-disable-next-line react/no-children-prop
+                              children={
+                                show ? (
+                                  <Eye
+                                    size={25}
+                                    style={{ alignSelf: "center" }}
+                                  />
+                                ) : (
+                                  <EyeClosed
+                                    size={25}
+                                    style={{ alignSelf: "center" }}
+                                  />
+                                )
+                              }
                             />
-                          ) : (
-                            <EyeClosed
-                              size={25}
-                              style={{ alignSelf: "center" }}
+                          </InputGroup>
+                          {errors.password && (
+                            <FormErrorMessage ml={5}>
+                              {errors?.password?.message}
+
+                            </FormErrorMessage>
+                          )}
+                        </FormControl>
+                      )}
+                    />
+
+                    <Button type="submit" variant="outline" rightIcon={<SignIn size={18} />} color="primary.400" colorScheme="primary"  >
+                      Login
+                    </Button>
+                  </form>
+
+                </TabPanel>
+                <TabPanel>
+                  <form onSubmit={handleSubmit(onSignIn)}>
+
+                    <Controller
+                      name="login"
+                      control={control}
+                      rules={{ required: "Login obrigatório." }}
+                      render={({ field }) => (
+                        <FormControl isInvalid={errors?.login}>
+
+                          <Text color="dark.300">Login</Text>
+                          <InputGroup>
+                            <InputLeftElement pointerEvents='none'>
+                              {/* <LockKey size={76} /> */}
+                            </InputLeftElement>
+                            <Input focusBorderColor="primary.400" type='text' placeholder='Ex: lele123' {...field} />
+                          </InputGroup>
+
+                          {errors.login && (
+                            <FormErrorMessage ml={5}>
+                              {errors?.login?.message}
+                            </FormErrorMessage>
+                          )}
+                        </FormControl>
+                      )}
+                    />
+
+                    <Controller
+                      name="password"
+                      control={control}
+                      rules={{ required: "Senha é obrigatória." }}
+                      render={({ field }) => (
+                        <FormControl isInvalid={errors.password}>
+                          <Text color="dark.300">Senha</Text>
+                          <InputGroup>
+                            <InputLeftElement color="primary.400" pointerEvents='none'>
+                              <LockKey size={24} style={{ color: "primary.400" }} />
+                            </InputLeftElement>
+                            <Input focusBorderColor="primary.400" type={!show ? 'password' : "text"} placeholder='Digite sua senha' {...field} />
+                            <InputRightElement
+                              onClick={() => setShow((prev) => !prev)}
+                              color="primary.400"
+                              // eslint-disable-next-line react/no-children-prop
+                              children={
+                                show ? (
+                                  <Eye
+                                    size={25}
+                                    style={{ alignSelf: "center" }}
+                                  />
+                                ) : (
+                                  <EyeClosed
+                                    size={25}
+                                    style={{ alignSelf: "center" }}
+                                  />
+                                )
+                              }
                             />
-                          )
-                        }
-                      />
-                    </InputGroup>
-                    {errors.password && (
-                      <FormErrorMessage ml={5}>
-                        {errors?.password?.message}
+                          </InputGroup>
+                          {errors.password && (
+                            <FormErrorMessage ml={5}>
+                              {errors?.password?.message}
 
-                      </FormErrorMessage>
-                    )}
-                  </FormControl>
-                )}
-              />
+                            </FormErrorMessage>
+                          )}
+                        </FormControl>
+                      )}
+                    />
 
-              <Button type="submit" variant="outline" rightIcon={<SignIn size={18} />} color="primary.400" colorScheme="primary"  >
-                Login
-              </Button>
-            </Stack>
-          </form>
+                    <Button type="submit" variant="outline" rightIcon={<SignIn size={18} />} color="primary.400" colorScheme="primary"  >
+                      Login
+                    </Button>
+                    {/* <p>two!</p> */}
+                  </form>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+
+          </Stack>
+
 
         </Flex>
-      </Flex>
+      </Flex >
     </>
   )
 }
