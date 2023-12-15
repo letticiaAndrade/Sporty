b02212165ca2e3ec4459367ebcab735580530b12
leticia <leticiaae@hotmail.com.br>
2023-12-15 17:02:22 -0300

    feat:tournament::added iniciate tournament page

---
+++ sporty/src/pages/Tournament.jsx
@@ -0,0 +1,8 @@
+import { Trophy } from "@phosphor-icons/react";
+import { TitlePage } from "../components";
+
+export function Tournament() {
+    return (
+        <TitlePage title={"Torneios"} Icon={Trophy}/>
+    )
+}
