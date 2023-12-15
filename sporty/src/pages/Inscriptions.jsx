1242c1401aa54e9281364f086edc0b41c05402c4
leticia <leticiaae@hotmail.com.br>
2023-12-15 17:02:03 -0300

    feat:inscriptions::added iniciate inscriptions page

---
+++ sporty/src/pages/Inscriptions.jsx
@@ -0,0 +1,8 @@
+import { IdentificationCard } from "@phosphor-icons/react";
+import { TitlePage } from "../components";
+
+export function Inscriptions() {
+    return (
+        <TitlePage title={"Inscrições"} Icon={IdentificationCard}/>
+    )
+}
