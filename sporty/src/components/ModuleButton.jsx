59433bca22a88677c4260a43ce76afe7b490b18b
leticia <leticiaae@hotmail.com.br>
2023-12-15 17:01:35 -0300

    feat:module::added module components

---
+++ sporty/src/components/ModuleButton.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import { Center, Stack, Text } from "@chakra-ui/react";
+
+export function ModuleButton({ label, Icon, onClick, comment = null }) {
+
+  return (
+    <Center
+      p={8}
+      mb={6}
+      bg="white"
+      boxShadow="md"
+      flexDir="column"
+      color="dark.300"
+      borderRadius={8}
+      opacity={1}
+      w={{ base: "80%", sm: 280 }}
+      onClick={onClick}
+      h={{ base: "80%", sm: 280 }}
+      cursor={"pointer"}
+      _hover={{
+        color: "primary.400",
+        transform: "scale(1.1)",
+      }}
+    >
+      <Icon size={60} />
+      <Stack space={0}>
+        <Text mt={6} textAlign="center" fontSize={16} fontWeight="medium">{label}</Text>
+        <Text mt={6} textAlign="center" fontSize={14} fontWeight="light">{comment}</Text>
+      </Stack>
+    </Center>
+  );
+}
---
+++ sporty/src/components/UserModules.jsx
@@ -0,0 +1,49 @@
+import { useMemo, useState } from "react";
+import { ModuleButton } from "./ModuleButton";
+import { useNavigate } from "react-router-dom";
+import {CirclesFour, IdentificationCard, Trophy} from "@phosphor-icons/react";
+
+export function UserModules() {
+    const navigation = useNavigate();
+    // const session = useCache(state => state.session);
+    const [ module ] = useState([
+        {
+            name: "tournaments",
+            render: (key) => <ModuleButton key={key}
+                Icon={Trophy}
+                label="Torneios"
+                comment="Gerencie seus Torneios"
+                onClick={() => navigation("/tournament")}
+            />
+        },
+        {
+            name: "categories",
+            render: (key) => <ModuleButton key={key}
+                Icon={CirclesFour}
+                label="Categorias"
+                comment="Gerencie suas Categorias"
+                onClick={() => navigation("/categories")}
+            />
+        },
+        {
+            name: "inscriptions",
+            render: (key) => <ModuleButton key={key}
+                Icon={IdentificationCard}
+                label="Inscrições"
+                comment="Gerencie suas Inscrições"
+                onClick={() => navigation("/inscriptions")}
+            />
+        },
+      
+    ]);
+
+    const modules = useMemo(() => {
+            return module.map(mod => {
+                    return mod.render(mod.name)
+            })
+        
+    }, [ module ]);
+
+    return modules;
+
+}
