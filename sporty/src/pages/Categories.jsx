aa14263941b88603a88f356d895574896a9fcb2d
leticia <leticiaae@hotmail.com.br>
2023-12-15 17:00:59 -0300

    feat:categories::added iniciate categorie page

---
+++ sporty/src/pages/Categories.jsx
@@ -0,0 +1,8 @@
+import { CirclesFour } from "@phosphor-icons/react";
+import { TitlePage } from "../components";
+
+export function Categories() {
+    return (
+        <TitlePage title={"Categorias"} Icon={CirclesFour}/>
+    )
+}
