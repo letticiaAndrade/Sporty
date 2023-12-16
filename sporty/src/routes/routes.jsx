9de957f39c49a0e0cb5d091d95594557646f25a7
leticia <leticiaae@hotmail.com.br>
2023-12-16 17:12:02 -0300

    feat:routes:: added new routes

--- sporty/src/routes/routes.jsx
+++ sporty/src/routes/routes.jsx
@@ -9,14 +9,18 @@ export function RoutesApp() {
 
             {/* TELAS REFERENTES A APLICAÇÃO */}
             <Route path="/home" element={<Home />} />
+
+            {/* TELAS REFERENTES AO TORNEIO */}
             <Route path="/tournament" element={<Tournament />} />
+            <Route path="/tournament/details" element={<Tournament />} />
+
+            {/* TELAS REFERENTES AS CATEGORIAS */}
             <Route path="/categories" element={<Categories />} />
+            <Route path="/categories/details" element={<Tournament />} />
+
+            {/* TELAS REFERENTES AS INSCRIÇÕES */}
             <Route path="/inscriptions" element={<Inscriptions />} />
-
-
-
-            
-
+            <Route path="/inscriptions/details" element={<Tournament />} />
         </Routes>
     )
 
