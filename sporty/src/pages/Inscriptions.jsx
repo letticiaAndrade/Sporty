b23fa9e8e3bc86807cc7a2b910623602a2923b84
leticia <leticiaae@hotmail.com.br>
2023-12-16 19:41:22 -0300

    feat:inscription:: iniciate back end integration

--- sporty/src/pages/Inscriptions.jsx
+++ sporty/src/pages/Inscriptions.jsx
@@ -2,16 +2,21 @@ import { CheckCircle, Plus, IdentificationCard } from "@phosphor-icons/react";
 import { Header, TitlePage, InscriptionCard } from "../components";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { Button, Center, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack, useDisclosure } from "@chakra-ui/react";
+import { Button, Center, Flex, FormControl, FormErrorMessage, FormLabel, Heading, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Select, Stack, useDisclosure } from "@chakra-ui/react";
 import routes from "../service/api";
 import { inscriptionsAPI } from "../service/apiExample";
+import { useCache } from "../hooks";
 
 export function Inscriptions() {
     const [inscriptions, setInscriptions] = useState(inscriptionsAPI);
+    // const { categories, setCache } = useCache();
+
 
     const { register, handleSubmit, formState: { errors } } = useForm({
         defaultValues: {
-            name: ""
+            cat_nr_id: "",
+            usu_nr_id1: "",
+            usu_nr_id2: "",
         }
     });
 
@@ -35,69 +40,90 @@ export function Inscriptions() {
 
     return (
         <Flex w="full" h="100vh">
-        <Header />
+            <Header />
 
-        <Flex flexDir="column" px={2} pb={8} flex={9} bgColor="light.100">
+            <Flex flexDir="column" px={2} pb={8} flex={9} bgColor="light.100">
 
-            <TitlePage title="Inscrições" Icon={IdentificationCard} />
+                <TitlePage title="Inscrições" Icon={IdentificationCard} />
 
-            <Stack align="center" justify="flex-end" direction={{ base: "column", sm: "row" }} mb={8}>
-                <Button
-                    size="md"
-                    my={2} px={10}
-                    colorScheme="primary"
-                    onClick={onOpenCreate}
-                    leftIcon={<Plus size={20} />}
-                    w={{ base: "full", sm: "fit-content" }}
-                >
-                    Nova Inscrição
-                </Button>
-            </Stack>
+                <Stack align="center" justify="flex-end" direction={{ base: "column", sm: "row" }} mb={8}>
+                    <Button
+                        size="md"
+                        my={2} px={10}
+                        colorScheme="primary"
+                        onClick={onOpenCreate}
+                        leftIcon={<Plus size={20} />}
+                        w={{ base: "full", sm: "fit-content" }}
+                    >
+                        Nova Inscrição
+                    </Button>
+                </Stack>
 
-            <Stack direction="row" flexWrap="wrap" mt={8} spacing={3}>
-                {inscriptions?.length
-                    ? inscriptions.map(inscriptions =>
-                        <InscriptionCard key={inscriptions?.ins_nr_id} inscription={inscriptions} />)
-                    : (
-                        <Center flexDir="column" w="full">
-                            {/* <Image src={nothing} w="80" mb={8} /> */}
-                            <Heading fontSize={24}>Nada aqui</Heading>
-                        </Center>
-                    )}
-            </Stack>
+                <Stack direction="row" flexWrap="wrap" mt={8} spacing={3}>
+                    {inscriptions?.length
+                        ? inscriptions.map(inscriptions =>
+                            <InscriptionCard key={inscriptions?.ins_nr_id} inscription={inscriptions} />)
+                        : (
+                            <Center flexDir="column" w="full">
+                                {/* <Image src={nothing} w="80" mb={8} /> */}
+                                <Heading fontSize={24}>Nada aqui</Heading>
+                            </Center>
+                        )}
+                </Stack>
 
-            <Modal isOpen={isOpenCreate} onClose={onCloseCreate}>
-                <ModalOverlay />
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <ModalContent bg="light.100">
-                        <ModalHeader>Nova Inscrição</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                            <Stack spacing={3}>
-                                <FormControl isRequired isInvalid={errors?.name}>
-                                    <FormLabel color="dark.100">Nome</FormLabel>
-                                    <Input
-                                        bg="#FFF" placeholder="Nome"
-                                        focusBorderColor="primary.400"
-                                        {...register("name", { required: "Nome é obrigatório." })}
-                                    />
-                                    <FormErrorMessage>{errors?.name?.message}</FormErrorMessage>
-                                </FormControl>
-                            </Stack>
-                        </ModalBody>
+                <Modal isOpen={isOpenCreate} onClose={onCloseCreate}>
+                    <ModalOverlay />
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <ModalContent bg="light.100">
+                            <ModalHeader>Nova Inscrição</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                <Stack spacing={3}>
 
-                        <ModalFooter>
-                            <Button variant="ghost" colorScheme="error" mr={3} onClick={onCloseCreate}>
-                                Cancelar
-                            </Button>
-                            <Button type="submit" colorScheme="success" leftIcon={<CheckCircle size={20} />}>
-                                Cadastrar Inscrição
-                            </Button>
-                        </ModalFooter>
-                    </ModalContent>
-                </form>
-            </Modal>
+                                    <FormControl isRequired isInvalid={errors?.cat_nr_id}>
+                                        <FormLabel color="dark.100">Categoria</FormLabel>
+                                        <Input
+                                            bg="#FFF" placeholder="Identificador da categoria "
+                                            focusBorderColor="primary.400"
+                                            {...register("cat_nr_id", { required: "Identificador da categoria é obrigatório." })}
+                                        />
+                                        <FormErrorMessage>{errors?.cat_nr_id?.message}</FormErrorMessage>
+                                    </FormControl>
+
+                                    <FormControl isRequired isInvalid={errors?.usu_nr_id1}>
+                                        <FormLabel color="dark.100">Primeiro Participante</FormLabel>
+                                        <Input
+                                            bg="#FFF" placeholder="Identificador do participante"
+                                            focusBorderColor="primary.400"
+                                            {...register("usu_nr_id1", { required: "Identificador do participante é obrigatório." })}
+                                        />
+                                        <FormErrorMessage>{errors?.usu_nr_id1?.message}</FormErrorMessage>
+                                    </FormControl>
+
+                                    <FormControl isRequired isInvalid={errors?.usu_nr_id2}>
+                                        <FormLabel color="dark.100">Segundo Participante</FormLabel>
+                                        <Input
+                                            bg="#FFF" placeholder="Identificador do participante"
+                                            focusBorderColor="primary.400"
+                                            {...register("usu_nr_id2", { required: "Identificador da categoria é obrigatório." })}
+                                        />
+                                        <FormErrorMessage>{errors?.usu_nr_id2?.message}</FormErrorMessage>
+                                    </FormControl>
+                                </Stack>
+                            </ModalBody>
+
+                            <ModalFooter>
+                                <Button variant="ghost" colorScheme="error" mr={3} onClick={onCloseCreate}>
+                                    Cancelar
+                                </Button>
+                                <Button type="submit" colorScheme="success" leftIcon={<CheckCircle size={20} />}>
+                                    Cadastrar Inscrição
+                                </Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </form>
+                </Modal>
+            </Flex>
         </Flex>
-    </Flex>
     )
 }
